b4fe2500d43113cb663f322ea5ae40ae10c68c5d
Arpita <62317642+arpitamittal@users.noreply.github.com>
2022-03-25 09:32:29 +0530

    Time: 35 ms (11.39%), Space: 37.1 MB (7.53%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int dp[10002];
-        vector<int> freq(10002);
-        for(auto i : nums) freq[i]++;
-        
-        dp[1] = freq[1]; 
-        dp[2] = max(freq[1], 2*freq[2]);
-        for(int i=3; i<10002; i++) dp[i] = max(dp[i-1], dp[i-2]+freq[i]*i);
-        
-        return dp[10001];
+        vector<int> freq(100001);
+        int maxi = 0;
+        unordered_map<int, int> mp;
+        for(auto num : nums)
+        {
+            freq[num]++;
+            maxi = max(maxi, num);
+        }
+        return points(freq, 0, maxi, mp);
+    }
+    int points( vector<int> &freq, int current, int maxi, unordered_map<int, int> &mp)
+    {
+        if(current > maxi)
+            return 0;
+        int currentKey = current;
+        if(mp.find(currentKey) != mp.end())
+            return mp[currentKey];
+        int earn = freq[current]*current + points(freq, current+2, maxi, mp);
+        int skip = points(freq, current+1, maxi, mp);
+        mp[currentKey] = max(earn, skip);
+        return mp[currentKey];
     }
 };
