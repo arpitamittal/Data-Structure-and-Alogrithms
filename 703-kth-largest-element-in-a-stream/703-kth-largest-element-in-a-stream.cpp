b2bb35a0f13ece2b453ad921d642e0436a8c721c
Arpita <62317642+arpitamittal@users.noreply.github.com>
2022-04-08 23:35:09 +0530

    Time: 1779 ms (5.02%), Space: 22.2 MB (5.09%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,22 +1,17 @@
 class KthLargest {
-public:
-    priority_queue<int, vector<int>, greater<int>> pq; //Min heap
+    multiset<int> s;
     int size;
+public:
     KthLargest(int k, vector<int>& nums) {
         size = k;
-        for(int i=0; i<nums.size(); i++)
-        {
-            pq.push(nums[i]);
-            if(pq.size() > k)
-                pq.pop();
-        }
+        for(auto x : nums)   s.insert(x);
     }
     
     int add(int val) {
-        pq.push(val);
-        if(pq.size() > size)
-            pq.pop();
-        return pq.top();
+        s.insert(val);
+        auto it = s.begin();
+        for(int i = 0 ; i < (s.size()-size) ;i++) it++;
+        return *it;
     }
 };
 
