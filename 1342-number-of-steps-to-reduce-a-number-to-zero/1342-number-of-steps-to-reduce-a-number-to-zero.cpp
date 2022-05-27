add04e0395b62ede00ac01ec95b8f1cac643a0d1
Arpita <62317642+arpitamittal@users.noreply.github.com>
2022-05-28 00:51:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int count = 0;
-        while(num > 0)
-        {
-            if(num % 2 == 0) 
-                num /= 2;
-            else
-                num -= 1;
-            count++;
-        }
-        return count;
+        if(num == 0) return 0;
+        if(num == 1) return 1;
+        return numberOfSteps(num/2) + (num%2 == 0 ? 1 : 2);
     }
 };
