2135551e58d1d77ba83963e50c939864cbadca6d
Arpita <62317642+arpitamittal@users.noreply.github.com>
2022-06-04 20:15:38 +0530

    Time: 298 ms (42.93%), Space: 39.5 MB (30.80%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,38 +2,31 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int totalSum = 0;
-        
-        for(auto num : nums)
+        for(int num : nums)
             totalSum += num;
-        
-        if(totalSum % 2 != 0)
+        if(totalSum % 2 != 0) 
             return false;
         unordered_map<string, bool> mp;
-        return isPossible(nums, 0, totalSum/2, mp);
+        return isPossible(nums, totalSum/2, 0, mp);
     }
-    
-    int isPossible(vector<int> &nums, int current, int target, unordered_map<string, bool> &mp)
+    int isPossible(vector<int> &nums, int target, int currentIndex, unordered_map<string, bool> &mp)
     {
         if(target == 0)
             return true;
-        if(current >= nums.size())
+        if(currentIndex >= nums.size())
             return false;
-        string currentKey = to_string(current) + to_string(target);
-        
+        string currentKey = to_string(target) + " " + to_string(currentIndex);
         if(mp.find(currentKey) != mp.end())
             return mp[currentKey];
-        
-        bool consider = false;  
-        if(nums[current] <= target)
-            consider = isPossible(nums, current+1, target - nums[current], mp);
-        
+        bool consider = false;
+        if(nums[currentIndex] <= target)
+            consider = isPossible(nums, target - nums[currentIndex], currentIndex+1, mp);
         if(consider)
         {
             mp[currentKey] = consider;
             return true;
         }
-        
-        int notConsider = isPossible(nums, current+1, target, mp);
+        int notConsider = isPossible(nums, target, currentIndex+1, mp);
         mp[currentKey] = consider || notConsider;
         return mp[currentKey];
     }
